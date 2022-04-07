bfdc30e3ac4580f44aff6eb2adcca2c5f19787aa
Jesse Toivanen <jesse.toivanen2@gmail.com>
2022-04-07 14:45:55 +0300

    Add reset button to CreatNew, ex 7.5 & 7.6

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -71,16 +71,22 @@ const CreateNew = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content: content.value,
-      author: author.value,
-      info: info.value,
+      content: content.input.value,
+      author: author.input.value,
+      info: info.input.value,
       votes: 0
     })
     navigate('/')
-    props.setNotification(`a new anecdote ${content.value} created!`)
+    props.setNotification(`a new anecdote ${content.input.value} created!`)
     setTimeout(() => {
       props.setNotification('')
     }, 5000)
+  }
+
+  const handleReset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
   }
 
   return (
@@ -89,17 +95,20 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input {...content} />
+          <input {...content.input} />
         </div>
         <div>
           author
-          <input {...author} />
+          <input {...author.input} />
         </div>
         <div>
           url for more info
-          <input {...info} />
+          <input {...info.input} />
         </div>
         <button type="submit">create</button>
+        <button type="button" onClick={handleReset}>
+          reset
+        </button>
       </form>
     </div>
   )
